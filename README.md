add344dd8b38867cb853f582fc161633d45654e4
webfils <webfils@hotmail.com>
2021-07-02 19:21:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#### https://flask.palletsprojects.com/
+#### https://tailwindcss.com
+#### https://aprendejavascript.org/
+#### https://es.reactjs.org/
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    title = "Avro.dev"
+    return render_template('home.html', title=title)
+
+@app.route('/<error>')
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html', error=error), 404
+
+# Routes Example
+# --------------
+@app.route('/user/<string:user>')
+def user(user):
+   return "Hi "+ user
+
+@app.route('/num/<int:n>')
+def num(n):
+    return "Number: {}".format(n)
+
+@app.route('/user/<int:id>/<string:username>')
+def username(id, username):
+    return "ID: {}, Username: {}".format(id, username)
+
+@app.route('/suma/<float:n1>/<float:n2>')
+def suma(n1, n2):
+    return "Result: {}".format(n1 + n2)
+
+@app.route('/default/')
+@app.route('/default/<string:dft>')
+def dft(dft="default view"):
+    return "Value for default is: " + dft
+# --------------
+
+if __name__ == '__main__':
+    # Note:
+    # debug=True, port=1234, host='0.0.0.0'
+    app.run(debug=True)
